59104ba011b5ddbd3994263d9c28396f31caa91d
DeveloperC <DeveloperC@protonmail.com>
2019-03-11 20:59:02 +0000

    Compares two investments

--- Pension-Projection.py
+++ Pension-Projection.py
@@ -1,5 +1,8 @@
 #!/bin/python
 import locale
+import sys
+
+locale.setlocale( locale.LC_ALL, '' )
 
 def projection(monthlyContribution, duration, TER, yearlyGrowth):
     total = 0 
@@ -12,5 +15,17 @@ def projection(monthlyContribution, duration, TER, yearlyGrowth):
         total = total * (1-(TER/100))
     return total
 
-locale.setlocale( locale.LC_ALL, '' )
-print ("\nTotal -\n", locale.currency(projection(100,35,1.25,8), grouping=True));
+print("-- Constants --")
+print("Monthly Contribution: ",float(sys.argv[1]))
+print("Duration: ",int(sys.argv[2]))
+print("")
+print("-- Investment 1 --")
+print("Total: ", locale.currency(projection(float(sys.argv[1]), int(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4])), grouping=True));
+print("Expected Growth: ",sys.argv[4],"%")
+print("Total Charges: ",sys.argv[3],"%")
+print("")
+print("-- Investment 2 --")
+print("Total: ", locale.currency(projection(float(sys.argv[1]), int(sys.argv[2]), float(sys.argv[5]), float(sys.argv[6])), grouping=True));
+print("Expected Growth: ",sys.argv[6],"%")
+print("Total Charges: ",sys.argv[5],"%")
+

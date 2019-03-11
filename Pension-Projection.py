05426109e8875b4ed68291085f8393b9b0491d16
DeveloperC <DeveloperC@protonmail.com>
2019-03-11 21:17:16 +0000

    added graphing

--- Pension-Projection.py
+++ Pension-Projection.py
@@ -1,31 +1,29 @@
 #!/bin/python
-import locale
 import sys
+import matplotlib.pyplot as plt
 
-locale.setlocale( locale.LC_ALL, '' )
+years = range(0, int(sys.argv[2]))
 
 def projection(monthlyContribution, duration, TER, yearlyGrowth):
-    total = 0 
+    total = 0
+    stats = []
     
     for year in range(0, duration):
+        stats.append(total)
         total = total + (monthlyContribution*12)
         # Adding expected growth 
         total = total * (1+(yearlyGrowth/100))
         # Subtracting total expense ration (TER)
         total = total * (1-(TER/100))
-    return total
+    
+    return stats
 
-print("-- Constants --")
-print("Monthly Contribution: ",float(sys.argv[1]))
-print("Duration: ",int(sys.argv[2]))
-print("")
-print("-- Investment 1 --")
-print("Total: ", locale.currency(projection(float(sys.argv[1]), int(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4])), grouping=True));
-print("Expected Growth: ",sys.argv[4],"%")
-print("Total Charges: ",sys.argv[3],"%")
-print("")
-print("-- Investment 2 --")
-print("Total: ", locale.currency(projection(float(sys.argv[1]), int(sys.argv[2]), float(sys.argv[5]), float(sys.argv[6])), grouping=True));
-print("Expected Growth: ",sys.argv[6],"%")
-print("Total Charges: ",sys.argv[5],"%")
+investment1 = (projection(float(sys.argv[1]), int(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4])));
+investment2 = (projection(float(sys.argv[1]), int(sys.argv[2]), float(sys.argv[5]), float(sys.argv[6])));
 
+plt.plot(years, investment1)
+plt.plot(years, investment2)
+plt.xlabel('Duration (Years)')
+plt.ylabel('Total (£)')
+plt.grid(True)
+plt.show()
